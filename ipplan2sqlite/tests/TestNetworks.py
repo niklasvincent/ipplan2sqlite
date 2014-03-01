31a38570419fdf43f06bc92b8de3e104b1224bc1
Niklas Lindblad <nlindblad@tech.dreamhack.se>
2014-03-01 14:55:59 +0000

    Add unit tests for networks module. Add makefile for running tests.

---
+++ ipplan2sqlite/tests/TestNetworks.py
@@ -0,0 +1,55 @@
+import os
+import sqlite3
+import sys
+import unittest
+
+path = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../lib' ) )
+sys.path.insert( 1, path )
+import networks
+import tables
+
+class TestNetworks(unittest.TestCase):
+
+  def _query(self, q):
+    return self.c.execute( q ).fetchall()
+
+  def setUp(self):
+    self.conn = sqlite3.connect(':memory:')
+    self.c = self.conn.cursor()
+    tables.create( self.conn )
+
+  def tearDown(self):
+    self.conn.close()
+    self.c = None
+
+  def testAddAll(self):
+    networks.add_all( self.c )
+    nbr_of_networks = self._query( 'SELECT COUNT(*) FROM network' )[0][0]
+    self.assertEquals( nbr_of_networks, 4, "Additional or missing networks" )
+
+    print nbr_of_networks
+
+  def testAddAllRFC1918(self):
+    networks.add_all_rfc_1918( self.c )
+    expected_networks = [ '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16' ]
+    expected_netmasks = ['255.0.0.0', '255.240.0.0', '255.255.0.0']
+    actual_networks = self._query( 'SELECT * FROM network' )
+    self.assertEquals( len( actual_networks ), len( expected_networks ), "Additional or missing networks" )
+  
+    for network in actual_networks:
+      self.assertTrue( network[5] in expected_networks, "Network not amongst expected network" )
+      self.assertEquals( network[8], expected_netmasks[expected_networks.index( network[5] )], "Wrong netmask" )
+
+  def testAddAny(self):
+    networks.add_any( self.c )
+    actual_networks = self._query( 'SELECT * FROM network' )
+    self.assertEquals( len( actual_networks ), 1, "Additional or missing networks" )
+    self.assertEquals( actual_networks[0][1], "ANY" )
+    self.assertEquals( actual_networks[0][5], "0/0" )
+    self.assertEquals( actual_networks[0][6], "::/0" )
+
+def main():
+  unittest.main()
+
+if __name__ == '__main__':
+  main()
